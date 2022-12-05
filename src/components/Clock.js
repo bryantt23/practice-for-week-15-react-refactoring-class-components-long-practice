fd650acbc1938c2bf93ae1db6736b51c31d84d22
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-12-04 20:11:51 -0800

    refactor ClockToggle

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,72 +1,60 @@
 import React from 'react';
 
-export class ClockToggle extends React.Component {
-  render () {
-    return (
-      <button 
-        type="button"
-        className="clock-toggle" 
-        onClick={this.props.toggleClock}
-      >
-        Toggle Clock
-      </button>
-    )
-  }
-} 
+export function ClockToggle({ toggleClock }) {
+  return (
+    <button type='button' className='clock-toggle' onClick={toggleClock}>
+      Toggle Clock
+    </button>
+  );
+}
 
 class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: new Date(),
+      time: new Date()
     };
   }
-  
+
   componentDidMount() {
     this.interval = setInterval(this.tick, 1000);
   }
-  
+
   componentWillUnmount() {
-    console.log("Clearing Clock interval!")
+    console.log('Clearing Clock interval!');
     clearInterval(this.interval);
   }
-  
+
   tick = () => {
     this.setState({ time: new Date() });
-  }
+  };
 
   render() {
     let hours = this.state.time.getHours();
     let minutes = this.state.time.getMinutes();
     let seconds = this.state.time.getSeconds();
-    hours = (hours < 10) ? `0${hours}` : hours;
-    minutes = (minutes < 10) ? `0${minutes}` : minutes;
-    seconds = (seconds < 10) ? `0${seconds}` : seconds;
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
 
     const timezone = this.state.time
       .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
-      .replace(/[^A-Z]/g, "") // Strip out all but capitals
+      .replace(/[^A-Z]/g, '') // Strip out all but capitals
       .slice(3); // Eliminate initial GMT
 
     return (
-      <section className="clock-section">
+      <section className='clock-section'>
         <h1>Clock</h1>
         <div className='clock'>
           <p>
-            <span>
-              Time:
-            </span>
+            <span>Time:</span>
             <span>
               {hours}:{minutes}:{seconds} {timezone}
             </span>
           </p>
           <p>
-            <span>
-              Date: 
-            </span>
-            <span>
-              {this.state.time.toDateString()}
-            </span>
+            <span>Date:</span>
+            <span>{this.state.time.toDateString()}</span>
           </p>
         </div>
       </section>
